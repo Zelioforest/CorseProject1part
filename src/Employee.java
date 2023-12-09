7b5590a1c5a775f4aa55ec1dedd41efcd419ea34
Zelioforest <veshky@mail.ru>
2023-12-09 21:37:34 +0300

    easy

---
+++ src/Employee.java
@@ -0,0 +1,61 @@
+public class Employee {
+    public static int count;
+    int id;
+    private String name;
+    private String surname;
+    private String lastName;
+    private int department;
+    private int salary;
+
+    public Employee(String name, String surname, String lastName, int department, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.lastName = lastName;
+        this.department = department;
+        this.salary = salary;
+        this.id = count +1;
+        count += 1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return  "name = '" + name + '\'' +
+                ", surname = '" + surname + '\'' +
+                ", lastName = '" + lastName + '\'' +
+                ", department = " + department +
+                ", salary = " + salary;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,75 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int size = 10;
+        Employee[] worker = new Employee[10];
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        worker[0] = new Employee("Ivan", "Petrovich", "Calamstin", 3, 3568);
+        worker[1] = new Employee("Iva", "Petrovich", "Colvi", 3, 8960);
+        worker[2] = new Employee("Nina", "Petrovich", "Recartin", 2, 7640);
+        worker[3] = new Employee("Igor", "Petrovich", "Restart", 1, 8890);
+        worker[4] = new Employee("Svetlana", "Petrovich", "Maliko", 5, 6798);
+        worker[5] = new Employee("Maria", "Petrovich", "Neelova", 5, 7754);
+        worker[6] = new Employee("Oleg", "Petrovich", "Xoftin", 5, 8862);
+        worker[7] = new Employee("Ivan", "Petrovich", "Zernov", 1, 5120);
+        worker[8] = new Employee("Irvud", "Petrovich", "Nemo", 3, 4586);
+        worker[9] = new Employee("Stas", "Petrovich", "Mordred", 3, 7652);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        int lenEmployee = worker.length;
+        makeListEmployee(worker, lenEmployee);
+        int sumSalary = calculateSumSalary(worker, lenEmployee);
+        System.out.println("sum = " + sumSalary);
+        findMaxSalary(worker, lenEmployee);
+        findMinSalary(worker, lenEmployee);
+        calculateAvgSalary(sumSalary, lenEmployee);
+        getListFio(worker, lenEmployee);
+    }
+
+    public static void makeListEmployee (Employee[] worker, int len) {
+        for (int i = 0; i < len; i++) {
+            System.out.print(worker[i].getId() + "  ");
+            System.out.println(worker[i]);
         }
+    }
+
+    public static int calculateSumSalary(Employee[] worker, int len) {
+        int sum = 0;
+        for (int k = 0; k < len; k++){
+            sum += worker[k].getSalary();
+        }
+        return sum;
+    }
+
+    public static void findMinSalary(Employee[] worker, int len) {
+        int min = worker[0].getSalary();
+        for (int k = 1; k < len; k++){
+            if (min > worker[k].getSalary()) {
+                min = worker[k].getSalary();
+            }
+        }
+        System.out.println("min = " + min);
+    }
+
+    public static void findMaxSalary(Employee[] worker, int len) {
+        int max = worker[0].getSalary();
+        for (int k = 1; k < len; k++){
+            if (max < worker[k].getSalary()) {
+                max = worker[k].getSalary();
+            }
+        }
+        System.out.println("max = " + max);
+    }
+
+    public static void calculateAvgSalary(int s, int l) {
+        float avgSalary = s / l;
+        System.out.println("avgSalary = " + avgSalary);
+    }
+
+    public static void getListFio(Employee[] worker, int len) {
+        for (int i = 0; i < len; i++) {
+            System.out.println((i + 1) + "  " + worker[i].getName() + ' ' +
+                    worker[i].getSurname() + ' ' + worker[i].getLastName());
+        }
+
     }
 }
